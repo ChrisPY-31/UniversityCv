60f3434503162456d68147711db8bd76f7c91a9c
ChrisDev <Christianpeya@hotmail.com>
2025-04-27 09:00:02 -0600

    first commit

---
+++ src/main/java/com/chris/userporfiles/UserPorfilesApplication.java
@@ -0,0 +1,13 @@
+package com.chris.userporfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserPorfilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserPorfilesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chris/userporfiles/UserPorfilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chris.userporfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserPorfilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
